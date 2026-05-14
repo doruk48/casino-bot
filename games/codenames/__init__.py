40d44bed49aded6f747b4f86e3f3be7ba65cc9ac
doruk48 <serkoo120@gmail.com>
2026-05-15 01:46:43 +0300

    Create __init__.py

---
+++ games/codenames/__init__.py
@@ -0,0 +1,6 @@
+# games/codenames/__init__.py
+
+def register_handlers(app):
+    """Tüm Codenames handler'larını bot uygulamasına kaydeder."""
+    # Şimdilik boş, handler'ları yazınca doldurulacak
+    pass
