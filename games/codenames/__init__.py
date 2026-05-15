2bea7c1c55727dbf84463c33cc86cae128d3312f
doruk48 <serkoo120@gmail.com>
2026-05-16 00:30:23 +0300

    Update __init__.py

--- games/codenames/__init__.py
+++ games/codenames/__init__.py
@@ -1,8 +1,1 @@
 from .handlers import register_handlers
-
-# games/codenames/__init__.py
-
-def register_handlers(app):
-    """Tüm Codenames handler'larını bot uygulamasına kaydeder."""
-    # Şimdilik boş, handler'ları yazınca doldurulacak
-    pass
